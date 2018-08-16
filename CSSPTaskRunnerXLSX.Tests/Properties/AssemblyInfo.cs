ff2e74e44377a5dc838956d7eb54a764363cf3d3
ECCC-CSSP <charles.leblanc2@canada.ca>
2018-08-16 17:14:44 -0300

    Initial commit

---
+++ CSSPTaskRunnerXLSX.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSSPTaskRunnerXLSX.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Environment Canada")]
+[assembly: AssemblyProduct("CSSPTaskRunnerXLSX.Tests")]
+[assembly: AssemblyCopyright("Copyright © Environment Canada 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("991f0995-ce2c-4f3f-9faa-3dd9e6220509")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CSSPTaskRunnerXLSXHelper.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSSPTaskRunnerXLSXHelper.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Environment Canada")]
+[assembly: AssemblyProduct("CSSPTaskRunnerXLSXHelper.Tests")]
+[assembly: AssemblyCopyright("Copyright © Environment Canada 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e0a13ec8-53e2-4553-b86f-fae317574746")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
